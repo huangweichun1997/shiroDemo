ea7136467176170d6040c63e6717593b108a48b6
huangweichun1997 <huangweichun1997@gmail.com>
2023-09-20 22:13:55 +0800

    项目创建，springboot、swagger配置

---
+++ src/main/java/com/hwc/shiro_study/ShiroStudyApplication.java
@@ -0,0 +1,14 @@
+package com.hwc.shiro_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class ShiroStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroStudyApplication.class, args);
+    }
+
+}
